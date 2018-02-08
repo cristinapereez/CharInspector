16418a681d215d27f44ff28f6cd762538ac9f496
cristinapereez <cperez58@mail.csuchico.edu>
2018-02-08 11:46:55 -0800

    Input Characters

--- main.cpp
+++ main.cpp
@@ -5,20 +5,27 @@ using namespace std;
 
 int main()
 {
-
+char Letter;
   cout<<"What character do you want to know about?\n";
-
+cin>>Letter;
+if(Letter>='A' && Letter<='Z')
+{
   //when user's entry is between A-Z...
   cout<<" is an upper case letter!\n";
-
+}
+else if(Letter>='a' && Letter<='z')
+{
   //when user's entry is between a-z...
   cout<<" is a lower case letter!\n";
-
+}
+else
+{
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+ }
+ 
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int) Letter<<endl;
 
   return 0;
 }
